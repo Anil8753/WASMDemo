eaa8623f736d3ecb030b19f09445f12bb4b23cf4
Anil8753 <anilkumar8753@gmail.com>
2019-12-31 20:37:39 +0530

    call js from c++

--- CallJsFromCpp/src/main.cpp
+++ CallJsFromCpp/src/main.cpp
@@ -11,15 +11,6 @@ EM_JS(void, call_js, (), {
 
 bool callJsBack()
 {
-    emscripten::val Math = emscripten::val::global("Math");
-    int x = Math.call<int>("abs", -10);
-    std::cout << x << std::endl;
-
-    {
-        std::string msg = "Message from C++";
-        emscripten::val o = emscripten::val::global("jsMethodWithMsg");
-        o.call<int>(msg.c_str());
-    }
     call_js();
     return true;
 }
