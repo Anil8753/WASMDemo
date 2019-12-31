90f8a1d0f5e151c652e0bdb4e744b590a35bdb7a
Anil8753 <anilkumar8753@gmail.com>
2020-01-01 00:59:55 +0530

    callback demo

--- CallJsFromCpp/src/main.cpp
+++ CallJsFromCpp/src/main.cpp
@@ -5,6 +5,7 @@
 #include <emscripten/bind.h>
 #include <emscripten/val.h>
 
+//////////////////////////////////////////////////////////////////
 EM_JS(void, call_js, (), {
     jsMethod();
 });
@@ -15,7 +16,34 @@ bool callJsBack()
     return true;
 }
 
+//////////////////////////////////////////////////////////////////
+EM_JS(void, call_js_agrs, (const char *title, int lentitle, const char *msg, int lenmsg), {
+    jsMethodAgrs(UTF8ToString(title, lentitle), UTF8ToString(msg, lenmsg));
+});
+
+bool callJsBackWithAgrs()
+{
+    const std::string title = "Hello from C++";
+    const std::string msg = "This string is passed as a paramter from C++ code!";
+    call_js_agrs(title.c_str(), title.length(), msg.c_str(), msg.length());
+    return true;
+}
+
+//////////////////////////////////////////////////////////////////
+EM_JS(void, callback, (int index, const char *msg, int lenmsg), {
+    functionMap(index, UTF8ToString(msg, lenmsg));
+});
+
+void cppFunctionWithJsCallback(int index)
+{
+    const std::string msg = "Js callback called with this data from C++ code!";
+    callback(index, msg.c_str(), msg.length());
+}
+
+//////////////////////////////////////////////////////////////////
 EMSCRIPTEN_BINDINGS(module)
 {
     emscripten::function("callJsBack", &callJsBack);
+    emscripten::function("callJsBackWithAgrs", &callJsBackWithAgrs);
+    emscripten::function("cppFunctionWithJsCallback", &cppFunctionWithJsCallback);
 }
